de3d41b22f11a49568367c7924bce99e2a2387f6
andip71 <andreasp@gmx.de>
2015-03-30 17:21:18 +0200

    Boeffla ramfs: Adjustments for CM12.1 kernel

--- ramdisk_boeffla/fs/res/bc/bccontroller.sh
+++ ramdisk_boeffla/fs/res/bc/bccontroller.sh
@@ -11,7 +11,7 @@
 # ********************************
 
 # kernel specification (hardware; type; target; url)
-KERNEL_SPECS="i9300;cm;cm12.0;http://boeffla.df-kunde.de/sgs3/boeffla-kernel-cm-ng/"
+KERNEL_SPECS="i9300;cm;cm12.1;http://boeffla.df-kunde.de/sgs3/boeffla-kernel-cm-ng/"
 
 # kernel features 
 # (1=enable-busybox,2=enable-frandom,3=wipe-cache,4=disable-zram-control)
